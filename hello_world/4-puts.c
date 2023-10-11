4a09e54d2748ee31dcb7bbc3df7e873ef9eebc90
root <gwenaelholberton@gmail/com>
2023-10-11 00:34:10 -0700

    Write a C program that prints exactly sentence, followed by a new line.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * Description: C program that prints sentence, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
