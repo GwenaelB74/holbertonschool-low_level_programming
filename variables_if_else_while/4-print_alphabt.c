b7f2d01b1b7cff9a8a7f84d7ab84a79ce3a4fd8b
root <gwenaelholberton@gmail/com>
2023-10-16 05:25:22 -0700

    Alphabet without Q and E

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,8 @@
 int main(void)
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++) 
-	{
+	for (letter = 'a'; letter <= 'z'; letter++) {
+
 	if (letter != 'q' && letter != 'e')
 		{
 			putchar(letter);
