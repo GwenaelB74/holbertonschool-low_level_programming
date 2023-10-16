9c31af3ec180231a426c8aaa44f8ea245e79ad12
root <gwenaelholberton@gmail/com>
2023-10-16 06:52:04 -0700

    script that prints all possible combinations of single-digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Script that prints all combinaisons possible
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main() {
+    int i;
+    for (i = 0; i <= 9; i++) {
+        putchar(i + '0');
+
+        if (i < 9) {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+	putchar('\n');
+    return 0;
+}
