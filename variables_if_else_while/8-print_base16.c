4937847ed607afdbb09f963f65fe5d0eac91d2df
root <gwenaelholberton@gmail/com>
2023-10-16 06:42:25 -0700

    script that prints all the numbers of base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Script that prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void) 
+{
+	char numhexa = '0';
+    	char letter = 'a';
+
+	while (numhexa < '9')
+	{
+		putchar(numhexa);
+		numhexa++;
+	}
+	while (letter < 'g')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
