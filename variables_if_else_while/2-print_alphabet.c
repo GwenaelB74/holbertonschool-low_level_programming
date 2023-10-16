8079d2e8cbaa2555f0c3e6529d420e573f303397
root <gwenaelholberton@gmail/com>
2023-10-16 04:56:38 -0700

    reword Print alphabet in lowercase

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	char alphabet = 'a'; while 
-		(alphabet <= 'z') {
+	char alphabet = 'a';
+	
+	while (alphabet <= 'z') 
+	{
 		putchar(alphabet);
 		alphabet++;
-		}
+	}
 	putchar('\n');
 	return (0);
 }
