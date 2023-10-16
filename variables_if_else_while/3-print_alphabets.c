15d021f073554d41efc0ea2fb2c28a79a94cb39f
root <gwenaelholberton@gmail/com>
2023-10-16 05:09:10 -0700

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * Description: Script that print alphabet in lowercase AND in uppercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char lowercase ='a';
+	char uppercase ='A';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+
+	while (uppercase <= 'Z') 
+	{
+	putchar(uppercase);
+	uppercase++;
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
