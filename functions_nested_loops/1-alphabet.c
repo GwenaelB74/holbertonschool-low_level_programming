38ad63f2ed7e8cfab77fcbc63ec51d82795c2b43
root <gwenaelholberton@gmail/com>
2023-10-17 02:02:40 -0700

    Project 1, Task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * main - entry point
+ * 
+ * Description: Projet 2, Task 1
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
 void print_alphabet(void)
 {
 	char letter = 'a';
