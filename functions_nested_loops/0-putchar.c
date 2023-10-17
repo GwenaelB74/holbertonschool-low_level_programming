1e184253f0382e90963454858dd778d1d708a576
root <gwenaelholberton@gmail/com>
2023-10-17 00:28:07 -0700

    Adds script 0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Script that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
