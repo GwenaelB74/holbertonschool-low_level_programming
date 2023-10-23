b349f2b66da500fa00acd023f502a0c1823f1082
root <gwenaelholberton@gmail/com>
2023-10-23 06:18:11 -0700

    Projet 1, Task 7 3.0

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,27 +11,27 @@ void puts_half(char *str)
 {
 	int length = 0;
 	int start;
-	
+
 	while (str[length])
 	{
 		length++;
 	}
-	
+
 	if (length % 2 == 0)
 	{
 		start = length / 2;
 	}
-	
+
 	else
 	{
 		start = (length + 1) / 2;
 	}
-	
+
 	while (str[start] != '\0')
 	{
 		_putchar(str[start]);
 		start++;
 	}
-	
+
 	_putchar('\n');
 }
