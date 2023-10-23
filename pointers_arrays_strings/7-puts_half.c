fad2b2ed65984cb8eb87064a1c5eaa1410f04cfb
root <gwenaelholberton@gmail/com>
2023-10-23 06:02:01 -0700

    Projet 1, Task 7 2.0

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,31 +7,31 @@
  * Description: This function calculates the starting index based on the length
  * of the string and prints the characters from that index to the end.
  */
-
 void puts_half(char *str)
 {
-	int lenght = 0;
-	int start;
+    int length = 0;
+    int start;
 
-	while (str[lenght])
-	{
-		lenght++;
-	}
+    while (str[length])
+    {
+        length++;
+    }
 
-	if (lenght % 2 == 0)
-	{
-		start = lenght / 2;
-	}
-	else
-	{
-		start = (lenght - 1) / 2;
-	}
+    if (length % 2 == 0)
+    {
+        start = length / 2;
+    }
+    else
+    {
+        start = (length + 1) / 2;
+    }
 
-	while (str[start] != '\0')
-	{
-		_putchar(str[start]);
-		start++;
-	}
+    while (str[start] != '\0')
+    {
+        _putchar(str[start]);
+        start++;
+    }
 
-	_putchar('\n');
+    _putchar('\n');
 }
+
