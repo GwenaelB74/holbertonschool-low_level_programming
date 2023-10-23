f1faa4d16b280bc34c0f79a93aa1d3e58300d55c
root <gwenaelholberton@gmail/com>
2023-10-23 01:40:46 -0700

    Project 1, Task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * Main - entry point
+ *
+ * Description: function that takes a pointer to an int as parameter and updates the value it points to to 98
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
