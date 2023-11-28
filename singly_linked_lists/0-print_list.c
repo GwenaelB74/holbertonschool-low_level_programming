bea94ef969a42258d82cb5b0d5babc4d2df51531
root <gwenaelholberton@gmail/com>
2023-11-28 00:21:54 -0800

    project

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h) {
+	
+	size_t node_count = 0;
+
+	while (h != NULL) {
+	if (h->str == NULL) {
+		printf("[0] (nil)\n");
+	} else {
+		printf("[%lu] %s\n", h->len, h->str);
+	}
+
+	h = h->next;
+	node_count++;
+	}
+
+	return node_count;
+}
