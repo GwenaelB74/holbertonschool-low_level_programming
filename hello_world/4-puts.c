e40fd39b9884dd22047a4aa6aeead3c8d3456338
GwenaelB74 <userr.74140@gmail.com>
2024-03-06 01:52:16 +0000

    task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * description : task 1
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void) {
+	printf("Programming is like building a multilingual puzzle");
+	return(0);
+}
